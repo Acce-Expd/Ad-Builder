7c014203001e6b6fee4d3355673f9b4698d6b26a
dannyarodriguez <danny.accenture@outlook.com>
2022-03-22 16:11:41 -0600

    add js
    Generated the option for  create and copy urls, and clear the form

--- js/main.js
+++ js/main.js
@@ -1,52 +1,57 @@
-$(function(){
-    function list(c, n, z) {
-        s = document.form.marktext.value;
-        if (c.checked) {
-            if (s.indexOf(n) < 0) s += ', ' + n;
-        } else {
-            s = document.form.marktext.value.replace(', ' + n, '');
-        }
-        z = ", ";
-        if (s.substring(2) == z) s = s.substring(2);
-        document.form.marktext.value = s;
+function list(c, n, z) {
+    s = document.form.marktext.value;
+    if (c.checked) {
+        if (s.indexOf(n) < 0) s += ', ' + n;
+    } else {
+        s = document.form.marktext.value.replace(', ' + n, '');
     }
-
-    function getchecked() {
-        var newtxt = '';
-        var chkbx = document.getElementsByTagName('input');
-        for (var i = 0; i < chkbx.length; i++) {
-            if (chkbx[i].type == 'checkbox' && chkbx[i].checked === true) {
-                if (newtxt.length !== 0) {
-                    newtxt += ',';
-                }
-                newtxt += chkbx[i].value;
+    z = ", ";
+    if (s.substring(2) == z) s = s.substring(2);
+    document.form.marktext.value = s;
+}
+function getChecked() {
+    var newtxt = '';
+    var chkbx = document.getElementsByTagName('input');
+    for (var i = 0; i < chkbx.length; i++) {
+        if (chkbx[i].type == 'checkbox' && chkbx[i].checked === true) {
+            if (newtxt.length !== 0) {
+                newtxt += ',';
             }
+            newtxt += chkbx[i].value;
         }
-        document.form.marktext.value = 'https://s3.amazonaws.com/ewe-assets/media/meso_cm/others/Prototype/EN/index.html?prd=' + newtxt;
     }
+    document.form.marktext.value = '/index.html?prd=' + newtxt;
+}
 
-    function myFunction() {
-        var copyText = document.getElementById("myInput");
-        copyText.select();
-        document.execCommand("Copy");
+function copyFuntion() {
+    var copyText = document.getElementById("txtInput");
+    copyText.select();
+    document.execCommand("Copy");
 
-        var tooltip = document.getElementById("myTooltip");
-        tooltip.innerHTML = "URL Copied!"; // + copyText.value;
-    }
+    var tooltip = document.getElementById("copyTooltip");
+    tooltip.innerHTML = "URL Copied!"; // + copyText.value;
+}
 
-    function outFunc() {
-        var tooltip = document.getElementById("myTooltip");
-        tooltip.innerHTML = "Copy the generated URL";
-    }
+function outFunc() {
+    var tooltip = document.getElementById("copyTooltip");
+    tooltip.innerHTML = "Copy the generated URL";
+}
 
-    function testurl() {
-        var gettesturl = document.getElementById("myInput").value;
-        window.open(gettesturl);
-    }
+function testurl() {
+    var gettesturl = document.getElementById("txtInput").value;
+    window.open(gettesturl);
+}
+function clearForm() {
+    document.getElementById("itemsForm").reset();
+}
+
+$(function(){
+    
+
+    
+
+    
 
 
 
 });
-function clearForm() {
-    document.getElementById("itemsForm").reset();
-}
