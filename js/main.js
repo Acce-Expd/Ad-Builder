a4ff5d8d40f60b4bdb08c57a54f1e0aa50546ce8
dannyarodriguez <danny.accenture@outlook.com>
2022-03-24 23:21:10 -0600

    Generate the tab function
    Update the functionality  ton charge or hide the pages from tabs

--- js/main.js
+++ js/main.js
@@ -47,10 +47,50 @@ function clearForm() {
 
 $(function(){
 
+    
+    
     // Load htmls
     $('#1-1-ban').load ('./html/1-1-ban.html');    
     $('#1-2-btt').load ('./html/1-2-btt.html');
 
 
+    $('#tabs-bullets > .tab-links > li > a').click(function(e)  {
+        var currentAttrValue = $(this).attr('href');
+ 
+        // Show/Hide Tabs
+        $('#tabs-bullets ' + currentAttrValue).fadeIn(1000).siblings().hide();
+ 
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    }); 
+    
+    
+    (function() {
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+        var products = getParameterByName('prd');
+        products = products.split(',');
+        products.forEach((prd) => {
+            var el = document.getElementsByClassName(prd)[0];
+            el.style.display = 'block';        
+            $('#showD1-1').addClass('hide-select');
+            $('#showD1-2').addClass('hide-select');
+            $('#showD1-3').addClass('hide-select');
+            $('#showD1-4').addClass('hide-select');
+            $('#showD1-5').addClass('hide-select');
+            $('#showD1-6').addClass('hide-select');
+        });
+        var divact = products[0];
+        $('#' + divact + '').addClass('active').siblings().removeClass('active');
+    })();
 
 });
