f9463253802b21f199fb19303a61e989ed41c77e
dannyarodriguez <danny.accenture@outlook.com>
2022-03-29 13:37:05 -0600

    Light Box
    Modify the Light box function

--- js/lib/light-box.js
+++ js/lib/light-box.js
@@ -1,20 +1,20 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modalBan = document.getElementById("modalBan");
 var modalAd970x90 = document.getElementById("modalAd970x90");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var imgBan = document.getElementById("imgBan");
+var modalImgBan = document.getElementById("modalImgBan");
+var capBan = document.getElementById("capBan");
 
 var imgAd970x90 = document.getElementById("imgAd970x90");
 var modalImg970x90 = document.getElementById("modalImg970x90");
 var capAd970x90 = document.getElementById("capAd970x90");
 
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+imgBan.onclick = function(){
+  modalBan.style.display = "block";
+  modalImgBan.src = this.src;
+  capBan.innerHTML = this.alt;
 }
 
 imgAd970x90.onclick = function(){
@@ -24,8 +24,8 @@ imgAd970x90.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanBan = document.getElementsByClassName("closeBan")[0];
 var spanAd970x90 = document.getElementsByClassName("closeAd970x90")[0];
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {modal.style.display = "none";}
+spanBan.onclick = function() {modalBan.style.display = "none";}
 spanAd970x90.onclick = function() { modalAd970x90.style.display = "none";}
