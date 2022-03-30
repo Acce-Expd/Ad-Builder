4a51bf8e47c4e95259ea309520135557ce428a0b
dannyarodriguez <danny.accenture@outlook.com>
2022-03-29 22:45:24 -0600

    Fix modal bugs
    Update modal styles and fix some bugs

--- js/lib/light-box.js
+++ js/lib/light-box.js
@@ -1,6 +1,7 @@
 // Get the modal
 var modalBan = document.getElementById("modalBan");
 var modalAd970x90 = document.getElementById("modalAd970x90");
+var modalLpsp = document.getElementById("modalLpsp");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var imgBan = document.getElementById("imgBan");
@@ -10,6 +11,10 @@ var capBan = document.getElementById("capBan");
 var imgAd970x90 = document.getElementById("imgAd970x90");
 var modalImg970x90 = document.getElementById("modalImg970x90");
 var capAd970x90 = document.getElementById("capAd970x90");
+
+var imgLpsp = document.getElementById("imgLpsp");
+var modalImgLpsp = document.getElementById("modalImgLpsp");
+var capLpsp = document.getElementById("capLpsp");
 
 imgBan.onclick = function(){
   modalBan.style.display = "block";
@@ -23,9 +28,17 @@ imgAd970x90.onclick = function(){
   capAd970x90.innerHTML = this.alt;
 }
 
+imgLpsp.onclick = function(){
+  modalLpsp.style.display = "block";
+  modalImgLpsp.src = this.src;
+  capLpsp.innerHTML = this.alt;
+}
+
 // Get the <span> element that closes the modal
 var spanBan = document.getElementsByClassName("closeBan")[0];
 var spanAd970x90 = document.getElementsByClassName("closeAd970x90")[0];
+var spanLpsp = document.getElementsByClassName("closeLpsp")[0];
 // When the user clicks on <span> (x), close the modal
 spanBan.onclick = function() {modalBan.style.display = "none";}
-spanAd970x90.onclick = function() { modalAd970x90.style.display = "none";}
+spanAd970x90.onclick = function() {modalAd970x90.style.display = "none";}
+spanLpsp.onclick = function() {modalLpsp.style.display = "none";}
