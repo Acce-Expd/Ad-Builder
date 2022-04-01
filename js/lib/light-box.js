364064fea47cd1baddae000f21049e5497753027
dannyarodriguez <danny.accenture@outlook.com>
2022-04-01 01:41:51 -0600

    Update Styles in Native Placement Section
    Update Styles in Native Placement Section and add the light box function

--- js/lib/light-box.js
+++ js/lib/light-box.js
@@ -4,6 +4,9 @@ var modalIosaf = document.getElementById("modalIosaf");
 var modalCcmf = document.getElementById("modalCcmf");
 
 var modalAd970x90 = document.getElementById("modalAd970x90");
+var modalNmc = document.getElementById("modalNmc");
+var modalNts = document.getElementById("modalNts");
+var modalAd160x600 = document.getElementById("modalAd160x600");
 
 var modalLpsp = document.getElementById("modalLpsp");
 
@@ -23,6 +26,19 @@ var capCcmf = document.getElementById("capCcmf");
 var imgAd970x90 = document.getElementById("imgAd970x90");
 var modalImg970x90 = document.getElementById("modalImg970x90");
 var capAd970x90 = document.getElementById("capAd970x90");
+
+var imgNmc = document.getElementById("imgNmc");
+var modalImgNmc = document.getElementById("modalImgNmc");
+var capNmc = document.getElementById("capNmc");
+
+var imgNts = document.getElementById("imgNts");
+var modalImgNts = document.getElementById("modalImgNts");
+var capNts = document.getElementById("capNts");
+
+var imgAd160x600 = document.getElementById("imgAd160x600");
+var modalImg160x600 = document.getElementById("modalImg160x600");
+var capAd160x600 = document.getElementById("capAd160x600");
+
 
 var imgLpsp = document.getElementById("imgLpsp");
 var modalImgLpsp = document.getElementById("modalImgLpsp");
@@ -50,6 +66,24 @@ imgAd970x90.onclick = function(){
   capAd970x90.innerHTML = this.alt;
 }
 
+imgNmc.onclick = function(){
+  modalNmc.style.display = "block";
+  modalImgNmc.src = this.src;
+  capNmc.innerHTML = this.alt;
+}
+
+imgNts.onclick = function(){
+  modalNts.style.display = "block";
+  modalImgNts.src = this.src;
+  capNts.innerHTML = this.alt;
+}
+
+imgAd160x600.onclick = function(){
+  modalAd160x600.style.display = "block";
+  modalImg160x600.src = this.src;
+  capAd160x600.innerHTML = this.alt;
+}
+
 imgLpsp.onclick = function(){
   modalLpsp.style.display = "block";
   modalImgLpsp.src = this.src;
@@ -62,6 +96,9 @@ var spanIosaf = document.getElementsByClassName("closeIosaf")[0];
 var spanCcmf = document.getElementsByClassName("closeCcmf")[0];
 
 var spanAd970x90 = document.getElementsByClassName("closeAd970x90")[0];
+var spanNmc = document.getElementsByClassName("closeNmc")[0];
+var spanNts = document.getElementsByClassName("closeNts")[0];
+var spanAd160x600 = document.getElementsByClassName("closeAd160x600")[0];
 
 var spanLpsp = document.getElementsByClassName("closeLpsp")[0];
 
@@ -71,5 +108,8 @@ spanIosaf.onclick = function() {modalIosaf.style.display = "none";}
 spanCcmf.onclick = function() {modalCcmf.style.display = "none";}
 
 spanAd970x90.onclick = function() {modalAd970x90.style.display = "none";}
+spanNmc.onclick = function() {modalNmc.style.display = "none";}
+spanNts.onclick = function() {modalNts.style.display = "none";}
+spanAd160x600.onclick = function() {modalAd160x600.style.display = "none";}
 
 spanLpsp.onclick = function() {modalLpsp.style.display = "none";}
